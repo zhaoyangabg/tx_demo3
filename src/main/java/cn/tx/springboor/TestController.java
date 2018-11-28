ed284beb133d676324cdfd4bc3d34e0adad0b019
zhaoyangabg <417254084@qq.com>
2018-11-28 18:06:09 +0800

    Initial commit

---
+++ src/main/java/cn/tx/springboor/TestController.java
@@ -0,0 +1,12 @@
+package cn.tx.springboor;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello111888";
+    }
+}
---
+++ src/main/java/cn/tx/springboor/TxDemo3Application.java
@@ -0,0 +1,12 @@
+package cn.tx.springboor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TxDemo3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TxDemo3Application.class, args);
+    }
+}
